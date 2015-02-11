7e28f062e5dd834978d216d7957ed126b7571894
ZelteHonor <gabrieldionbouchard@gmail.com>
2015-02-11 17:01:00 -0500

    JavaFX first use

--- src/control/Game.java
+++ src/control/Game.java
@@ -1,6 +1,9 @@
 package control;
 
 import javafx.application.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Game extends Application {
@@ -11,9 +14,16 @@ public class Game extends Application {
 	}
 	
 	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void start(Stage primaryStage) throws Exception {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("View.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
