3ff77180cc17552b478fcac1e1ba045c0803b061
Antho15 <tyus1500@hotmail.com>
2015-02-11 16:12:18 -0500

    Application

--- src/control/Game.java
+++ src/control/Game.java
@@ -1,5 +1,14 @@
 package control;
 
-public class Game {
+import javafx.application.*;
+import javafx.stage.Stage;
+
+public class Game extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
