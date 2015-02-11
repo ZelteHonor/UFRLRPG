92d06fae3fb7f1ed8243dc8c643edf706efcc424
ngc7293 <dav.bour@gmail.com>
2015-02-11 16:21:12 -0500

    Rajouter le main

--- src/control/Game.java
+++ src/control/Game.java
@@ -4,7 +4,12 @@ import javafx.application.*;
 import javafx.stage.Stage;
 
 public class Game extends Application {
-
+	
+	public static void main(String args[]) {
+		System.out.println("GFY Oracle");
+		launch();
+	}
+	
 	@Override
 	public void start(Stage arg0) throws Exception {
 		// TODO Auto-generated method stub
