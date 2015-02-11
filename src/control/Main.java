3c1a716179628ae032ab4f72636090a79b6f2ce4
ZelteHonor <gabrieldionbouchard@gmail.com>
2015-02-11 17:08:27 -0500

    This

--- src/control/Main.java
+++ src/control/Main.java
@@ -6,7 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class Game extends Application {
+public class Main extends Application {
 	
 	public static void main(String args[]) {
 		System.out.println("FUCK YOU JAVA");
