0b97a22ca04961cb6d31e89f1c11109e5bee1ef3
ZelteHonor <gabrieldionbouchard@gmail.com>
2015-02-11 15:09:05 -0500

    initial push

---
+++ desktop/src/com/mygdx/Ufrlrpg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.Ufrlrpg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.Ufrlrpg.Ufrlrpg;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Ufrlrpg(), config);
+	}
+}
