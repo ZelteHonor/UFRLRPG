ebbdd97e570a14396b95e603a81f5f1186d65633
ZelteHonor <gabrieldionbouchard@gmail.com>
2015-02-18 17:57:44 -0500

    Canvas active

--- src/control/Controller.java
+++ src/control/Controller.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
@@ -14,6 +15,10 @@ public class Controller implements Initializable{
 	private BorderPane root;
 	@FXML
 	private Pane pane;
+	@FXML
+	private Pane gamePane;
+	@FXML
+	private Canvas view;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
--- src/control/Main.java
+++ src/control/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("View.fxml"));
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
