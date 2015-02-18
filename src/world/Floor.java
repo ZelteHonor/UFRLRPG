ced068fadb32a63ba09257b9f537c0423d8b5ef5
ngc7293 <dav.bour@gmail.com>
2015-02-18 13:49:57 -0500

    Ajouter Floor()

--- src/world/Floor.java
+++ src/world/Floor.java
@@ -1,5 +1,10 @@
 package world;
 
 public class Floor {
-	private World.TILE[][] tiles = new World.TILE[World.SIZE][World.SIZE];
+	private World.TILE[][] tiles;
+	private int depth;
+	
+	public Floor(int depth) {
+		tiles = new World.TILE[World.SIZE][World.SIZE];
+	}
 }
