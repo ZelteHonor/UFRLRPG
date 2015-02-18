4e8c394ce26c817a1575ecb6a9b034c845ea652a
David Janelle <djanelle@localhost.localdomain>
2015-02-18 16:10:37 -0500

    Modified the fxml file

--- src/control/Controller.java
+++ src/control/Controller.java
@@ -3,10 +3,15 @@ package control;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
 
 public class Controller implements Initializable{
 
+	@FXML 
+	private BorderPane root;
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
