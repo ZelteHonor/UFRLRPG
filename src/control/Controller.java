f55b507eec44d1510bdb2b61c82bf6b036ec0751
David Janelle <djanelle@localhost.localdomain>
2015-02-18 16:17:53 -0500

    fixed fxml file and controller

--- src/control/Controller.java
+++ src/control/Controller.java
@@ -6,11 +6,14 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 
 public class Controller implements Initializable{
 
 	@FXML 
 	private BorderPane root;
+	@FXML
+	private Pane pane;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
