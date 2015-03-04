9213fdc34bc611d2cb946db9d82c2872d14e2f06
Antho15 <tyus1500@hotmail.com>
2015-03-04 18:14:17 -0500

    Wall detection : unfinished
    Reste à supprimer les mur dans les mur!

--- src/render/Render.java
+++ src/render/Render.java
@@ -3,13 +3,26 @@
  */
 package render;
 
+import java.awt.Point;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+
+
+
+
+import sun.tools.jar.Main;
 import gameObjects.GameObjects;
+import world.Generator;
 import world.World;
+import world.World.TILE;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.RadialGradient;
+import javafx.scene.paint.Stop;
 
 /**
  * Cette classe dessine sur un Canvas le monde et ces objets.
@@ -34,6 +47,8 @@ public class Render {
 	
 	//Lien des Images
 	private HashMap<String,Image> spriteMap;
+	
+	Wall wall;
 
 	/**
 	 * Constructeur
@@ -46,12 +61,16 @@ public class Render {
 		this.world = world;
 		
 		DH = 11;//11
-		DW = 60;//20
+		DW = 20;//20
 		RESOLUTION = 64;
 		
-		GUI = new Canvas(DW*RESOLUTION, DH*RESOLUTION);
+		//GUI = new Canvas(DW*RESOLUTION, DH*RESOLUTION);
 		
-		initSpriteMap();
+		//initSpriteMap();
+		
+		wall = new Wall(world);
+		wall.recalculate(0, 0);
+		wall.afficher();
 	}
 	/**
 	 * Constructeur
@@ -127,6 +146,8 @@ public class Render {
 	 */
 	public void draw(GameObjects obj){
 		
+		GUI.getGraphicsContext2D().save();
+		
 		Image sprite = getSprite(obj);
 		
 		GUI.getGraphicsContext2D().translate(obj.getX(),obj.getY());
@@ -136,25 +157,6 @@ public class Render {
 		GUI.getGraphicsContext2D().drawImage(sprite, -(sprite.getWidth()/2), -(sprite.getHeight()/2));
 		
 		GUI.getGraphicsContext2D().restore();
-	}
-	
-	/**
-	 * Vérifie si le sprite existe dans spriteMap, sinon l'ajoute.
-	 * 
-	 * @param obj
-	 * @return L'image du sprite
-	 */
-	public Image getSprite(GameObjects obj)
-	{
-		Image sprite = spriteMap.get(obj.getSprite());
-		
-		if(sprite == null)
-		{
-			spriteMap.put(obj.getSprite(), new Image(obj.getSprite()));
-		}
-		
-		return sprite;
-		
 	}
 	
 	/**
@@ -168,6 +170,30 @@ public class Render {
 		{
 			draw(obj[i]);
 		}
+		
+	}
+
+	public void drawShadows()
+	{
+		/*Stop[] stops1 = {new Stop(0.6,Color.TRANSPARENT),new Stop(1.0,Color.BLACK)};
+		Stop[] stops2 = {new Stop(0.0,Color.BLACK),new Stop(1.0,Color.BLACK)};
+		
+		GUI.getGraphicsContext2D().save();
+		
+		//Ombrage général
+		//GUI.getGraphicsContext2D().setGlobalAlpha(0.3);
+		//GUI.getGraphicsContext2D().fillRect(0, 0, DW*RESOLUTION	,DH*RESOLUTION);
+		GUI.getGraphicsContext2D().restore();
+		
+		
+		//Ombrage radial
+		GUI.getGraphicsContext2D().setFill(new RadialGradient(30, 0, 0, 0, 1, true, CycleMethod.NO_CYCLE, stops1));
+		GUI.getGraphicsContext2D().fillOval(0, 0, 1000, 1000);
+		GUI.getGraphicsContext2D().restore();
+		GUI.getGraphicsContext2D().setFill(new RadialGradient(0, 0, 0, 0, 0.7, true, CycleMethod.NO_CYCLE, stops2));
+		//GUI.getGraphicsContext2D().fillOval(0, 0, 1000, 1000);
+*/		
+		
 		
 	}
 	
@@ -200,11 +226,6 @@ public class Render {
 				{
 					switch(world[i][j].ordinal())
 					{
-
-
-
-
-
 					
 					case 0 :
 						break;
@@ -232,6 +253,24 @@ public class Render {
 		
 	}
 	
+	/**
+	 * Vérifie si le sprite existe dans spriteMap, sinon l'ajoute.
+	 * 
+	 * @param obj
+	 * @return L'image du sprite
+	 */
+	public Image getSprite(GameObjects obj)
+	{
+		Image sprite = spriteMap.get(obj.getSprite());
+		
+		if(sprite == null)
+		{
+			spriteMap.put(obj.getSprite(), new Image(obj.getSprite()));
+		}
+		
+		return sprite;
+		
+	}
 	public GameObjects[] getObjects() {
 		return objects;
 	}
@@ -286,5 +325,144 @@ public class Render {
 		spriteMap.put("img/dirt.png", new Image("img/dirt.png"));//CAVE
 		spriteMap.put("img/plank.png", new Image("img/plank.png"));//DONJON
 		spriteMap.put("img/plank_alt.png", new Image("img/plank_alt.png"));//TUNNEL
+	}
+	
+	private class Wall
+	{
+		private World.TILE[][] world;
+		private ArrayList<Point[]> mur;
+		
+		public Wall(World.TILE[][] world) {
+			
+			this.world = world;
+			mur = new ArrayList<Point[]>();
+			
+		}
+		
+		public void recalculate(int x, int y)
+		{
+			System.out.println("¤-"+(x - (DW/2))+" "+(y - (DH/2))+"-¤");
+			
+			afficher();
+			
+			//Parcoure les cases autour d'un point (x,y)
+			for(int j = y - (DH/2);j <= y + (DH/2);j++)
+			{
+				for(int i = x - (DW/2);i <= x + (DW/2);i++)
+				{
+
+					try
+					{
+					//Vérifie si c'est un mur
+					if(world[i][j] == TILE.WALL || world[i][j] == TILE.ROCK)
+					{
+						
+						//Vérifie si elle est seule ou non
+						if((world[i][j+1] == TILE.WALL || world[i][j+1] == TILE.ROCK) || (world[i+1][j] == TILE.WALL || world[i+1][j] == TILE.ROCK))
+						{
+							
+						
+							if(world[i+1][j] == TILE.WALL || world[i+1][j] == TILE.ROCK)
+							{
+								
+								mur.add(new Point[]{new Point(i,j),leftBorder(i+1,j)});
+							
+							}
+						
+							if(world[i][j+1] == TILE.WALL || world[i][j+1] == TILE.ROCK)
+							{
+							
+								mur.add(new Point[]{new Point(i,j),underBorder(i,j+1)});
+							
+							}
+						}
+						else
+						{
+							System.out.println(i);
+							mur.add( new Point[]{ new Point(i,j), new Point(i,j) });
+							
+						}
+						
+						
+					}
+					
+				}catch(ArrayIndexOutOfBoundsException e){}
+			}
+			}
+			
+			deleteRepetitions();
+			
+			for(int i = 0;i < mur.size(); i++)
+			{
+				System.out.println("("+mur.get(i)[0].x+","+mur.get(i)[0].y+")-("+mur.get(i)[1].x+","+mur.get(i)[1].y+")");
+			}
+		}
+		
+		public void afficher()
+		{
+			String affiche = "";
+			
+			for(int j = 0;j < 64;j++)
+			{
+				for(int i = 0;i < 64;i++)
+				{
+					if(world[i][j] == TILE.WALL || world[i][j] == TILE.ROCK)
+					{
+						if(mur.contains(new Point(i,j))){
+						affiche += "O";}
+						else{
+						affiche += "¤";}
+					}
+					else
+					{
+						affiche += "¬";
+					}
+				}
+				
+				affiche += "\n";
+			}
+			
+			System.out.println(affiche);
+		}
+		
+		private Point leftBorder(int x, int y)
+		{
+			Point border = new Point(x,y);
+			
+			if(world[x+1][y] == TILE.WALL || world[x+1][y] == TILE.ROCK)
+			{
+				border = leftBorder(x+1,y);
+			}
+			
+			return border;
+	
+		}
+		
+		private Point underBorder(int x, int y)
+		{
+			Point border = new Point(x,y);
+			
+			if(world[x][y+1] == TILE.WALL || world[x][y+1] == TILE.ROCK)
+			{
+				border = underBorder(x,y+1);
+			}
+			
+			return border;
+	
+		}
+		
+		private void deleteRepetitions()
+		{
+			//à faire
+		}
+	}
+	
+	public static void main(String args[]) {
+		
+		Generator gen = new Generator();
+
+		Render render = new Render(gen.generate());
+
+		
 	}
 }
