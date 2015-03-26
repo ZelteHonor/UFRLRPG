fe0559b87aee626814fe57e027c5a354e2b01164
David Janelle <djanelle@localhost.localdomain>
2015-03-25 20:30:04 -0400

    Make the attribute point distribution working

--- src/control/Controller.java
+++ src/control/Controller.java
@@ -12,6 +12,9 @@ import render.Render;
 import world.Generator;
 import world.World;
 import javafx.application.Platform;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import javafx.event.EventHandler;
@@ -52,7 +55,7 @@ public class Controller implements Initializable {
 	private Label intellectP;
 	@FXML
 	private Label agilityP;
-	
+
 	@FXML
 	private Button strM;
 	@FXML
@@ -66,11 +69,10 @@ public class Controller implements Initializable {
 	@FXML
 	private Button aglL;
 
-
 	private World world;
-	
+
 	private Service<Void> update;
-	
+
 	private Service<Void> screenRefresh;
 
 	private Service<Void> timer;
@@ -78,12 +80,14 @@ public class Controller implements Initializable {
 	private ArrayList<GameObjects> objects;
 
 	private Player player;
-	
+
 	private Render render;
-	
+
 	private MouseEvent mouse;
 	private double relativeMousePX;
 	private double relativeMousePY;
+
+	private static SimpleStringProperty intP, strP, aglP, remP;
 
 	public enum KEYSTATE {
 		PRESSED, RELEASED, DOWN, UP
@@ -93,10 +97,29 @@ public class Controller implements Initializable {
 	public void quit() {
 		System.exit(0);
 	}
-	
-	public void statChooser(MouseEvent e)
-	{
+
+	@FXML
+	public void statChooser(MouseEvent e) {
 		Button b = (Button) e.getSource();
+		if (b == strM && Integer.parseInt(remP.get()) > 0) {
+			strP.set(Integer.toString(Integer.parseInt(strP.get()) + 1));
+			remP.set(Integer.toString(Integer.parseInt(remP.get()) - 1));
+		} else if (b == strL && Integer.parseInt(strP.get()) > 0) {
+			strP.set(Integer.toString(Integer.parseInt(strP.get()) - 1));
+			remP.set(Integer.toString(Integer.parseInt(remP.get()) + 1));
+		} else if (b == intM && Integer.parseInt(remP.get()) > 0) {
+			intP.set(Integer.toString(Integer.parseInt(intP.get()) + 1));
+			remP.set(Integer.toString(Integer.parseInt(remP.get()) - 1));
+		} else if (b == intL && Integer.parseInt(intP.get()) > 0) {
+			intP.set(Integer.toString(Integer.parseInt(intP.get()) - 1));
+			remP.set(Integer.toString(Integer.parseInt(remP.get()) + 1));
+		} else if (b == aglM && Integer.parseInt(remP.get()) > 0) {
+			aglP.set(Integer.toString(Integer.parseInt(aglP.get()) + 1));
+			remP.set(Integer.toString(Integer.parseInt(remP.get()) - 1));
+		} else if (b == aglL && Integer.parseInt(aglP.get()) > 0) {
+			aglP.set(Integer.toString(Integer.parseInt(aglP.get()) - 1));
+			remP.set(Integer.toString(Integer.parseInt(remP.get()) + 1));
+		}
 	}
 
 	/**
@@ -122,17 +145,20 @@ public class Controller implements Initializable {
 		stage.setScene(scene);
 		stage.show();
 	}
+
 	@FXML
 	public void initGame() {
 
-		player = new Player(1, 10, 0, 0, 0, 10, 10, 10, null);
+		player = new Player(1, 10, Integer.parseInt(intP.getValue()),
+				Integer.parseInt(strP.getValue()), Integer.parseInt(aglP
+						.getValue()), 10, 10, 10, null);
 		player.setX(64);
 		player.setY(64);
 		player.setSprite("img/gabriel.png");
 		player.setAngle(0);
 
 		render = new Render(world.getFloor(0).getTiles());
-		
+
 		gamePane.getChildren().add(render.getGUI());
 
 		gamePane.setOnKeyPressed(new EventHandler<KeyEvent>() {
@@ -156,7 +182,7 @@ public class Controller implements Initializable {
 				}
 			}
 		});
-		
+
 		gamePane.setOnKeyReleased(new EventHandler<KeyEvent>() {
 			public void handle(KeyEvent ke) {
 				if (ke.getCode() == KeyCode.W) {
@@ -178,61 +204,69 @@ public class Controller implements Initializable {
 				}
 			}
 		});
-		
+
 		gamePane.setOnMousePressed(new EventHandler<MouseEvent>() {
 
 			@Override
 			public void handle(MouseEvent mou) {
-				if(mou.getButton() == MouseButton.PRIMARY)
-				{
+				if (mou.getButton() == MouseButton.PRIMARY) {
 					System.out.println("left click");
 					player.setKeyState(4, KEYSTATE.DOWN);
 					System.out.println(player.getX());
-				}
-				else if(mou.getButton() == MouseButton.SECONDARY)
-				{
+				} else if (mou.getButton() == MouseButton.SECONDARY) {
 					System.out.println("right click");
 					player.setKeyState(5, KEYSTATE.DOWN);
 				}
-				
+
 			}
-			
 
 		});
 		gamePane.setOnMouseMoved(new EventHandler<MouseEvent>() {
 
 			@Override
 			public void handle(MouseEvent mou) {
-				
+
 				mouse = mou;
-				relativeMousePX = (render.getDW()*render.getRESOLUTION()/2) - mou.getSceneX();
-				relativeMousePY = (render.getDH()*render.getRESOLUTION()/2) - mou.getSceneY();
-				
-				//Mettre plus tard dans joueur.update()
-				player.setAngle(Math.toDegrees(Math.atan2(relativeMousePY,relativeMousePX))+180);//====================================================================La classe joueur à le défaut de ne pas voir ce qu'il faut dans le contrôleur!(À CHANGER)
+				relativeMousePX = (render.getDW() * render.getRESOLUTION() / 2)
+						- mou.getSceneX();
+				relativeMousePY = (render.getDH() * render.getRESOLUTION() / 2)
+						- mou.getSceneY();
+
+				// Mettre plus tard dans joueur.update()
+				player.setAngle(Math.toDegrees(Math.atan2(relativeMousePY,
+						relativeMousePX)) + 180);// ====================================================================La
+													// classe joueur à le défaut
+													// de ne pas voir ce qu'il
+													// faut dans le
+													// contrôleur!(À CHANGER)
 			}
-			
 
 		});
-		
+
 		gamePane.setOnMouseDragged(new EventHandler<MouseEvent>() {
 
 			@Override
 			public void handle(MouseEvent mou) {
-				
+
 				mouse = mou;
-				relativeMousePX = (render.getDW()*render.getRESOLUTION()/2) - mou.getSceneX();
-				relativeMousePY = (render.getDH()*render.getRESOLUTION()/2) - mou.getSceneY();
-				
-				//Mettre plus tard dans joueur.update()
-				player.setAngle(Math.toDegrees(Math.atan2(relativeMousePY,relativeMousePX))+180);//====================================================================La classe joueur à le défaut de ne pas voir ce qu'il faut dans le contrôleur!(À CHANGER)
+				relativeMousePX = (render.getDW() * render.getRESOLUTION() / 2)
+						- mou.getSceneX();
+				relativeMousePY = (render.getDH() * render.getRESOLUTION() / 2)
+						- mou.getSceneY();
+
+				// Mettre plus tard dans joueur.update()
+				player.setAngle(Math.toDegrees(Math.atan2(relativeMousePY,
+						relativeMousePX)) + 180);// ====================================================================La
+													// classe joueur à le défaut
+													// de ne pas voir ce qu'il
+													// faut dans le
+													// contrôleur!(À CHANGER)
 			}
-			
 
 		});
 
 		this.update = new GameTask();
-		
+
 		this.screenRefresh = new GameRender();
 
 		timer = new GameTimer();
@@ -245,42 +279,40 @@ public class Controller implements Initializable {
 	 * 
 	 * @return null
 	 */
-	private class GameTimer extends Service<Void>  {
+	private class GameTimer extends Service<Void> {
 		boolean t = true;
-		
-		public GameTimer() {}
-		
+
+		public GameTimer() {
+		}
 
 		@Override
 		protected Task<Void> createTask() {
-			
+
 			return new Task<Void>() {
-                protected Void call() throws Exception {
-                	
-                    while(t){
-                    	
-                		Platform.runLater(
-                				()->{
-                					
-                						update.cancel();
-                						update.reset();
-                						update.start();
-                					
-                						
-                						screenRefresh.cancel();
-                						screenRefresh.reset();
-                						screenRefresh.start();
-        							});
-                		try {
-                			Thread.sleep(17);
-                		} catch (InterruptedException e) {
-                			e.printStackTrace();
-                		}
-                		
-                    }
-                    return null;
-                }
-            };
+				protected Void call() throws Exception {
+
+					while (t) {
+
+						Platform.runLater(() -> {
+
+							update.cancel();
+							update.reset();
+							update.start();
+
+							screenRefresh.cancel();
+							screenRefresh.reset();
+							screenRefresh.start();
+						});
+						try {
+							Thread.sleep(17);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+
+					}
+					return null;
+				}
+			};
 		}
 	}
 
@@ -289,65 +321,77 @@ public class Controller implements Initializable {
 	 * jeu
 	 *
 	 */
-	private class GameTask extends Service<Void>  {
-		
+	private class GameTask extends Service<Void> {
 
 		@Override
 		protected Task<Void> createTask() {
-			
-			return new Task<Void>() {
-                protected Void call() throws Exception {
-                   	
-                	Platform.runLater(
-                			()->{
-                					try
-                					{
-                						player.update(world.getFloor(0));
-                						for (GameObjects o : objects) {
-                							o.update(world.getFloor(0)); 
-                						}
-                					}catch(NullPointerException e){}
-        						});
-                		
 
-                    return null;
-                }
-            };
+			return new Task<Void>() {
+				protected Void call() throws Exception {
+
+					Platform.runLater(() -> {
+						try {
+							player.update(world.getFloor(0));
+							for (GameObjects o : objects) {
+								o.update(world.getFloor(0));
+							}
+						} catch (NullPointerException e) {
+						}
+					});
+
+					return null;
+				}
+			};
 		}
 	}
-	
-	private class GameRender extends Service<Void>  {
-		
+
+	private class GameRender extends Service<Void> {
 
 		@Override
 		protected Task<Void> createTask() {
-			
-			return new Task<Void>() {
-                protected Void call() throws Exception {
-                   	
-                	Platform.runLater(
-                			()->{
-                					try
-                					{
-                						double cx = mouse.getSceneX() - (render.getGUI().getWidth()/2);
-                						double cy = mouse.getSceneY() - (render.getGUI().getHeight()/2);
-                						render.drawWorld(player.getX() + cx,player.getY() + cy);
-                						render.draw(player);
-                					}catch(NullPointerException e){}
-        						});
-                		
 
-                    return null;
-                }
-            };
+			return new Task<Void>() {
+				protected Void call() throws Exception {
+
+					Platform.runLater(() -> {
+						try {
+							double cx = mouse.getSceneX()
+									- (render.getGUI().getWidth() / 2);
+							double cy = mouse.getSceneY()
+									- (render.getGUI().getHeight() / 2);
+							render.drawWorld(player.getX() + cx, player.getY()
+									+ cy);
+							render.draw(player);
+						} catch (NullPointerException e) {
+						}
+					});
+
+					return null;
+				}
+			};
 		}
 	}
-	
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		if (location.toString().contains("Game.fxml"))
 			world = new World();
+
+		if (location.toString().contains("CharacterCreation.fxml")) {
+			strP = new SimpleStringProperty();
+			strP.set("5");
+			strengthP.textProperty().bind(strP);
+			intP = new SimpleStringProperty();
+			intP.set("5");
+			intellectP.textProperty().bind(intP);
+			aglP = new SimpleStringProperty();
+			aglP.set("5");
+			agilityP.textProperty().bind(aglP);
+			remP = new SimpleStringProperty();
+			remP.set("30");
+			totalP.textProperty().bind(remP);
+		}
+
 	}
 
 }
