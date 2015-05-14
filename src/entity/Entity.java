7ba2bec87fade9b5f4e491b1a072b8a3c764f7aa
ZelteHonor <gabrieldionbouchard@gmail.com>
2015-05-14 15:07:55 -0400

    Comment Entity and Monster

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -3,11 +3,31 @@ package entity;
 import world.Floor;
 import gameobject.GameObject;
 
+/**
+ *	Classe abstraite pour les objets "vivant" (Monstre et Joueur)
+ */
 public abstract class Entity extends GameObject {
 
+	/**
+	 * Vie de l'entité
+	 */
 	protected int health;
+	
+	/**
+	 * Vie maximal de l'entité
+	 */
 	protected final int MAX_HEALTH;
 	
+	/**
+	 * Constructeur pour les entité.
+	 * @param x
+	 * 	X d'origine
+	 * @param y
+	 * 	Y d'origine
+	 * 
+	 * @param health
+	 * 	Vie d'origine (et donc maximal)
+	 */
 	public Entity(double x, double y, int health) {
 		super(x, y);
 		this.health = health;
@@ -15,10 +35,20 @@ public abstract class Entity extends GameObject {
 		
 	}
 	
+	/**
+	 * Retourne la vie présente de l'entité
+	 * @return
+	 * 	Le nombre de point de vie
+	 */
 	public int getHealth() {
 		return health;
 	}
 
+	/**
+	 * Change la vie d'une entité
+	 * @param health
+	 * 	La nouvelle vie
+	 */
 	public void setHealth(int health) {
 
 		if(health > MAX_HEALTH)
