7274a58495dda1cd4a434c05fe36a7d60afe35a2
ZelteHonor <gabrieldionbouchard@gmail.com>
2015-05-19 12:48:31 -0400

    Add comment in Artefact

--- src/pickups/Artefact.java
+++ src/pickups/Artefact.java
@@ -4,13 +4,26 @@ import control.Audio;
 import control.Controller;
 import world.Floor;
 
+/**
+ * Classe qui représente l'artefact sur la carte.
+ */
 public class Artefact extends PickUps {
 
+	/**
+	 * Constructeur de l'objet artefact
+	 * @param x
+	 * 	Position en X
+	 * @param y
+	 * 	Position en Y
+	 */
 	public Artefact(double x, double y) {
 		super(x, y);
 		sprite ="img/artefact.png";
 	}
 
+	/** 
+	 * Joue un son lorsque l'artefact est récupérer
+	 */
 	@Override
 	protected void activateEffect(Floor floor) {
 		floor.getPlayer().setArtefact(true);
